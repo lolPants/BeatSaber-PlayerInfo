047a8fe75dbae6382eb83d32ae006cb0c70db25b
Jack Baron <jackmbaron@gmail.com>
2018-10-11 09:41:28 +0100

    Implement default manager

---
+++ PlayerInfo/Misc/DefaultInfoManager.cs
@@ -0,0 +1,23 @@
+﻿using PlayerInfoPlugin.Misc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerInfoPlugin.Misc
+{
+    class DefaultInfoManager : IInfoManager
+    {
+        public string Platform => "Default";
+
+        public string Username { get; private set; }
+        public ulong UserID { get; private set; }
+
+        public void LoadPlayerInfo()
+        {
+            Username = "Player";
+            UserID = 0;
+        }
+    }
+}
