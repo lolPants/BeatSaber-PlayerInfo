e0c6ff84860806080a9697b741d2eeebca4bf5c1
Jack Baron <jackmbaron@gmail.com>
2018-10-11 09:33:52 +0100

    Plugin Project

---
+++ PlayerInfo/Plugin.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine.SceneManagement;
+using IllusionPlugin;
+
+namespace PlayerInfoPlugin
+{
+    public class Plugin : IPlugin
+    {
+        public string Name => "PlayerInfo";
+        public string Version => "0.1.0";
+
+        public void OnApplicationStart()
+        {
+            SceneManager.sceneLoaded += SceneLoaded;
+        }
+
+        private void SceneLoaded(Scene scene, LoadSceneMode arg1)
+        {
+
+        }
+
+        public void OnApplicationQuit()
+        {
+            SceneManager.sceneLoaded -= SceneLoaded;
+        }
+
+        public void OnFixedUpdate() { }
+
+        public void OnLevelWasInitialized(int level) { }
+
+        public void OnLevelWasLoaded(int level) { }
+
+        public void OnUpdate() {}
+    }
+}
