995e46606d00128ee80e0132551ccd1b9724e7e3
Jack Baron <jackmbaron@gmail.com>
2018-10-11 09:35:26 +0100

    Add infomanager interface

---
+++ PlayerInfo/Misc/IInfoManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PlayerInfoPlugin.Misc
+{
+    public interface IInfoManager
+    {
+        string Platform { get; }
+
+        string Username { get; }
+        ulong UserID { get; }
+
+        void LoadPlayerInfo();
+    }
+}
