24fec119ce0965d276e2c22f182e3c2039479f4f
Jack Baron <jackmbaron@gmail.com>
2018-10-11 09:47:43 +0100

    Implement Steamworks proof

--- PlayerInfo/Steam/InfoManager.cs
+++ PlayerInfo/Steam/InfoManager.cs
@@ -10,8 +10,15 @@ namespace PlayerInfoPlugin.Steam
 
         public string Username { get; private set; }
         public ulong UserID { get; private set; }
+        public string Proof { get; private set; }
 
         public event Action<string, ulong> PlayerInfoLoaded;
+        public event Action<string> PlayerProofGenerated;
+
+        private byte[] _ticketBlob = new byte[1024];
+        private uint _ticketSize;
+
+        private Callback<GetAuthSessionTicketResponse_t> m_GetAuthSessionTicketResponse;
 
         public void LoadPlayerInfo()
         {
@@ -22,6 +29,22 @@ namespace PlayerInfoPlugin.Steam
 
                 PlayerInfoLoaded.Invoke(Username, UserID);
             }
+        }
+
+        public void RequestPlayerProof()
+        {
+            Proof = null;
+
+            if (m_GetAuthSessionTicketResponse == null)
+                m_GetAuthSessionTicketResponse = Callback<GetAuthSessionTicketResponse_t>.Create(OnGetAuthSessionTicketResponse);
+
+            SteamUser.GetAuthSessionTicket(_ticketBlob, 1024, out _ticketSize);
+        }
+
+        private void OnGetAuthSessionTicketResponse(GetAuthSessionTicketResponse_t result)
+        {
+            string steamSessionticket = BitConverter.ToString(_ticketBlob, 0, (int)_ticketSize).Replace("-", "").ToLowerInvariant();
+            PlayerProofGenerated.Invoke(steamSessionticket);
         }
     }
 }
